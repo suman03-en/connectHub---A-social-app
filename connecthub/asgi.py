0f8ef7d9c87d7075edc698af26973f45a9272444
suman phuyal <sfuyal59@gmail.com>
2025-12-02 22:42:00 +0545

    upadated

--- connecthub/asgi.py
+++ connecthub/asgi.py
@@ -12,7 +12,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-
+from django.core.asgi import get_asgi_application 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connecthub.settings')
 
